e527630994772b8e8a9a48a70e187ae0f8f8abae
vishalok <vrsafetech@gmail.com>
2023-02-21 21:05:41 +0530

    Setup for basic user schema with pass encryption done

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -15,6 +15,19 @@ const userSchema = new mongoose.Schema({
         required:true
     }
 },{timestamps:true});
+
+userSchema.pre('save', async function encryptPassword(next){
+    const user = this;
+    const hash = await bcrypt.hash(this.password, 10); // length of password should be
+    this.password = hash;
+    next();
+});
+
+userSchema.method.isValidPassword = async function checkValidity(password){
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+    return compare;
+}
 
 const User = mongoose.model('User', userSchema);
 
